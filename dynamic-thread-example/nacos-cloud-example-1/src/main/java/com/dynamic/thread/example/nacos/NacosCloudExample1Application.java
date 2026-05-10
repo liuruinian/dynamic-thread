0f717cc09224477cd8e755cd1e5f974bda3fbe17
liuruinian <15852922408@139.com>
2026-05-10 11:22:29 +0800

    FIX: Example Issues.

---
+++ dynamic-thread-example/nacos-cloud-example-1/src/main/java/com/dynamic/thread/example/nacos/NacosCloudExample1Application.java
@@ -0,0 +1,15 @@
+package com.dynamic.thread.example.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Nacos Cloud Example 1 Application
+ */
+@SpringBootApplication
+public class NacosCloudExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosCloudExample1Application.class, args);
+    }
+}
